308511d22b0a1eaf01d3fde30db146b6f1f5d3a1
BuildTools <DeoTimeTheGithubUser@users.noreply.github.com>
2023-01-27 15:57:24 -0600

    Javaparser bean

--- src/main/kotlin/me/deo/dekotpiler/spring/DekotSpringConfiguration.kt
+++ src/main/kotlin/me/deo/dekotpiler/spring/DekotSpringConfiguration.kt
@@ -1,5 +1,6 @@
 package me.deo.dekotpiler.spring
 
+import com.github.javaparser.JavaParser
 import kotlinx.metadata.jvm.KotlinClassMetadata
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,4 +9,8 @@ import org.springframework.context.annotation.Configuration
 internal class DekotSpringConfiguration {
     @Bean
     fun kotlinMetadataReader() = KotlinClassMetadata.Companion
+
+    // Might need configuration here at some point
+    @Bean
+    fun javaParser() = JavaParser()
 }
