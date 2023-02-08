4caacec2691520e01a4102e4ed99ad48a63b6922
BuildTools <DeoTimeTheGithubUser@users.noreply.github.com>
2023-02-08 15:53:33 -0600

    Add to crawler

--- src/main/kotlin/me/deo/dekotpiler/crawler/StatementCrawler.kt
+++ src/main/kotlin/me/deo/dekotpiler/crawler/StatementCrawler.kt
@@ -1,4 +1,21 @@
 package me.deo.dekotpiler.crawler
 
+import me.deo.dekotpiler.model.KtStatement
+import me.deo.dekotpiler.model.statements.KtBodyStatement
+
 interface StatementCrawler {
+
+    fun Context.crawl()
+
+    interface Context {
+        fun pop(statement: KtStatement)
+        fun replace(old: KtStatement, new: KtStatement)
+        fun find(condition: (KtStatement) -> Boolean, closure: (KtStatement) -> Unit)
+
+        companion object {
+            inline fun <reified T : KtStatement> Context.find(crossinline closure: (T) -> Unit) = find({ it is T }) {
+                closure(it as T)
+            }
+        }
+    }
 }
