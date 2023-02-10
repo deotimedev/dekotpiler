19cb0964867dc1d1df8df79aad0d0c733fccab9b
deotime <89555032+DeoTimeTheGithubUser@users.noreply.github.com>
2023-02-09 20:20:42 -0600

    update

--- src/main/kotlin/me/deo/dekotpiler/crawler/impl/CrawlerControllerImpl.kt
+++ src/main/kotlin/me/deo/dekotpiler/crawler/impl/CrawlerControllerImpl.kt
@@ -3,6 +3,7 @@ package me.deo.dekotpiler.crawler.impl
 import me.deo.dekotpiler.crawler.Crawler
 import me.deo.dekotpiler.crawler.CrawlerController
 import me.deo.dekotpiler.model.statements.KtBlockStatement
+import me.deo.dekotpiler.model.statements.KtMultiBodyStatement
 import org.springframework.stereotype.Component
 
 @Component
@@ -10,7 +11,19 @@ internal class CrawlerControllerImpl(
     override val crawlers: List<Crawler>
 ) : CrawlerController {
     override fun deploy(crawler: Crawler, block: KtBlockStatement) {
-        val cursor = CrawlerCursorImpl(block)
-        with(crawler) { cursor.crawl() }
+        println("${block.statements.size} statements")
+        val paths = paths(block)
+        paths.forEach {
+            println("PATH: $it with ${it.size}")
+        }
     }
+
+    fun paths(thing: KtBlockStatement): List<List<KtBlockStatement>> = (listOf(listOf(thing)) + thing.statements.map { item ->
+        when (item) {
+            is KtMultiBodyStatement -> item.bodies.flatMap { paths(it) }.map { path ->
+                listOf(thing) + path
+            }
+            else -> listOf()
+        }
+    }.flatten())
 }
