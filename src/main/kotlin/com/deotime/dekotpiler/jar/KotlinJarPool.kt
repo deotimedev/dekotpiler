3dce8f82bfbeecd598591e56da0b9edfde4eae6e
BuildTools <DeoTimeTheGithubUser@users.noreply.github.com>
2023-03-01 10:39:31 -0600

    Update jar pool

--- src/main/kotlin/com/deotime/dekotpiler/jar/KotlinJarPool.kt
+++ src/main/kotlin/com/deotime/dekotpiler/jar/KotlinJarPool.kt
@@ -3,11 +3,7 @@ package com.deotime.dekotpiler.jar
 import com.deotime.dekotpiler.model.type.KtReferenceType
 import org.benf.cfr.reader.entities.ClassFile
 
-interface KotlinJarPool {
-    fun load(type: KtReferenceType): ClassFile?
-    fun locate(type: String): KtReferenceType?
-    fun locateJar(type: KtReferenceType): KotlinJar?
+interface KotlinJarPool : KotlinJar {
     fun standardLibrary(): KotlinJar
-
     fun register(jar: KotlinJar)
 }
--- src/main/kotlin/com/deotime/dekotpiler/jar/impl/KotlinJarPoolImpl.kt
+++ src/main/kotlin/com/deotime/dekotpiler/jar/impl/KotlinJarPoolImpl.kt
@@ -3,29 +3,35 @@ package com.deotime.dekotpiler.jar.impl
 import com.deotime.dekotpiler.jar.KotlinJar
 import com.deotime.dekotpiler.jar.KotlinJarPool
 import com.deotime.dekotpiler.model.type.KtReferenceType
+import kotlinx.metadata.jvm.KotlinClassMetadata
+import org.benf.cfr.reader.entities.ClassFile
 import org.springframework.stereotype.Component
 
 @Component
 internal class KotlinJarPoolImpl : KotlinJarPool {
 
     private val jars = mutableSetOf<KotlinJar>()
-    override fun locate(type: String) =
-        jars.firstNotNullOfOrNull {
-            it.type(type)
-        }
-
-    override fun load(type: KtReferenceType) =
-        jars.find { type in it }?.load(type)
-
-    override fun locateJar(type: KtReferenceType) =
-        jars.find { type in it }
+    override fun standardLibrary(): KotlinJar {
+        TODO("Not yet implemented")
+    }
 
     override fun register(jar: KotlinJar) {
         jars += jar
     }
 
-    override fun standardLibrary(): KotlinJar {
-        TODO("Not yet implemented")
-    }
+    override val types: List<KtReferenceType>
+        get() = jars.flatMap { it.types }
+
+    override fun type(name: String) =
+        jars.firstNotNullOfOrNull { it.type(name) }
+
+    override fun load(type: KtReferenceType) =
+        jars.firstNotNullOfOrNull { it.load(type) }
+
+    override fun metadata(type: KtReferenceType) =
+        jars.firstNotNullOfOrNull { it.metadata(type) }
+
+    override fun contains(type: KtReferenceType) =
+        jars.any { type in it }
 
 }
