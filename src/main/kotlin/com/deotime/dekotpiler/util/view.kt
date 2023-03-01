0fe82ab07115cb1603c764d36aca76d1fc5fa5c8
BuildTools <DeoTimeTheGithubUser@users.noreply.github.com>
2023-03-01 10:34:31 -0600

    Update view

--- src/main/kotlin/com/deotime/dekotpiler/util/view.kt
+++ src/main/kotlin/com/deotime/dekotpiler/util/view.kt
@@ -1,12 +1,11 @@
+@file:Suppress("UNCHECKED_CAST")
+
 package com.deotime.dekotpiler.util
 
 import kotlin.reflect.KClass
 import kotlin.reflect.KMutableProperty0
 import kotlin.reflect.KProperty0
 
-// todo will probably change this later but just want to get the base impl in for now
-
-// this is likely to be replaceable with List<View<T, Either<out F, MutableList<out F>>>
 typealias ViewableList<T, F> = List<Either<View<T, out F>, View<T, MutableList<out F>>>>
 
 class View<T, F>(
@@ -15,9 +14,10 @@ class View<T, F>(
     val set: (F) -> Unit
 )
 
-fun <T, F> T.view(focus: KProperty0<out F>) = View(this, focus.getter,
+fun <T, F> T.view(focus: KProperty0<F>) = View(
+    this, focus.getter,
     ((focus as? KMutableProperty0<out F>)?.setter ?: { error("Setted the unsettable") }) as (F) -> Unit
-) // fixme
+)
 
 fun <T, F> T.views(vararg focuses: KProperty0<*>): ViewableList<T, F> =
     focuses.map { field ->
@@ -36,9 +36,11 @@ inline fun <T, reified L, reified R, S> T.viewEither(prop: KMutableProperty0<Eit
     },
     {
         prop.set(
-            if (it is L) left(it)
-            else if (it is R) right(it)
-            else error("Wrong either type")
+            when (it) {
+                is L -> left(it)
+                is R -> right(it)
+                else -> error("Bad either type")
+            }
         )
     }
 )
