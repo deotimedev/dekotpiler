04897aaf9f1689b3a82640b170b9b06079d61f7b
deotime <89555032+DeoTimeTheGithubUser@users.noreply.github.com>
2023-03-10 13:00:17 -0600

    Updates

--- src/main/kotlin/com/deotime/dekotpiler/util/view.kt
+++ src/main/kotlin/com/deotime/dekotpiler/util/view.kt
@@ -12,10 +12,6 @@ interface View<out T> {
     fun get(): T
     fun <Key> unlock(type: KType, closure: (Unlocked<Key>) -> Unit)
 
-    operator fun <Key> Unlocked.Companion.invoke(set: (Key) -> Unit): Unlocked<Key> =
-        object : View<Key> by (this@View as View<Key>), Unlocked<Key> {
-            override fun set(value: @UnsafeVariance Key) = set(value)
-        }
 
     companion object {
         inline fun <reified Key> View<*>.unlock(noinline closure: (Unlocked<Key>) -> Unit) =
@@ -29,12 +25,22 @@ interface View<out T> {
     }
 
     class Simple<T>(
-        private val prop: KMutableProperty0<@UnsafeVariance T>
+        private val _get: () -> T,
+        private val _set: (T) -> Unit,
+        private val type: KType
     ) : View<T> {
-        override fun get() = prop.get()
+        constructor(prop: KMutableProperty0<T>) : this(prop::get, prop::set, prop.returnType)
+
+        override fun get() = _get()
         override fun <Key> unlock(type: KType, closure: (Unlocked<Key>) -> Unit) {
-            if (type >= prop.returnType)
-                closure(Unlocked((prop as KMutableProperty0<Key>)::set))
+            if (type >= this.type)
+                closure(object : View<Key> by (this@Simple as View<Key>), Unlocked<Key> {
+                    override fun set(value: @UnsafeVariance Key) =
+                        (_set as (Key) -> Unit)(value)
+                })
         }
     }
 }
+
+fun <T> views(vararg props: KMutableProperty0<T>) =
+    props.map { View.Simple(it) }
