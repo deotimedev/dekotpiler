d49c12630880d233be26b91c9e584c68e846566f
BuildTools <DeoTimeTheGithubUser@users.noreply.github.com>
2023-03-21 15:13:17 -0500

    inline

--- src/main/kotlin/com/deotime/dekotpiler/matching/Matchers.kt
+++ src/main/kotlin/com/deotime/dekotpiler/matching/Matchers.kt
@@ -1,28 +1,30 @@
+@file:Suppress("NOTHING_TO_INLINE")
 package com.deotime.dekotpiler.matching
 
 import com.deotime.dekotpiler.model.expressions.invoke.KtStaticInvoke
 
 object Matchers {
 
-    fun <T> everything() = Matcher<T> { true }
-    fun <T> nothing() = Matcher<T> { false }
+    inline fun <T> everything() = Matcher<T> { true }
+    inline fun <T> nothing() = Matcher<T> { false }
 
-    fun <T, V> value(reference: (T) -> V, vararg values: V) =
+    inline fun <T, V> value(crossinline reference: (T) -> V, vararg values: V) =
         Matcher<T> { reference(this) in values }
 
-    fun <T, V> notNull(reference: (T) -> V) = !value(reference, null)
+    inline fun <T, V> notNull(crossinline reference: (T) -> V) =
+        !value(reference, null)
 
     inline fun <reified T> staticFunction(vararg names: String) =
         Matcher<KtStaticInvoke> {
             function.enclosing.qualifiedName == T::class.qualifiedName && name in names
         }
 
-    fun <T> Matcher<T>.match(value: T) = value.match()
+    inline fun <T> Matcher<T>.match(value: T) = value.match()
 
     private fun <T> Matcher<T>.transform(other: Matcher<T>, boolOp: (Boolean, Boolean) -> Boolean) =
         Matcher<T> { boolOp(match(), other.match(this)) }
 
-    private fun <T> Matcher<T>.transform(boolOp: (Boolean) -> Boolean) =
+    private inline fun <T> Matcher<T>.transform(crossinline boolOp: (Boolean) -> Boolean) =
         Matcher<T> { boolOp(match()) }
 
     infix fun <T> Matcher<T>.and(other: Matcher<T>) = Matcher<T> { match() && other.match(this) }
